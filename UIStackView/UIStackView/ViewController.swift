e8f2fb41d8cf297f8edd16a33983096d6b6aba7f
Boxuan Zhu <BoxuanZhu@vlan-2658-10-16-238-187.student.wireless.sydney.edu.au>
2017-05-22 16:50:29 +1000

    first version for simplie UI Stack with minor bug

---
+++ UIStackView/UIStackView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UIStackView
+//
+//  Created by Boxuan Zhu on 5/22/17.
+//  Copyright © 2017 Boxuan Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topBlock: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topBlock.isHidden = false
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
